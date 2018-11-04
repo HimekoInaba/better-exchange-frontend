080e7e0016909497c9c8b11dda70667dc5515a87
Syrym <justsyrym@gmail.com>
2018-11-04 18:38:23 +0600

    [feat]initial commit

---
+++ src/containers/Topnav/Topnav.js
@@ -0,0 +1,43 @@
+import React, { Component } from 'react';
+import { Nav, Navbar, NavItem, NavDropdown, MenuItem } from "react-bootstrap"
+import './Topnav.scss';
+
+export default class Topnav extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+    };
+  }
+
+  render() {
+    return (
+      <div className="topnav_wrap">
+        <div className="topnav">
+          <Navbar>
+            <Navbar.Header>
+              <Navbar.Brand>
+                <a href="#home">React-Bootstrap</a>
+              </Navbar.Brand>
+            </Navbar.Header>
+            <Nav>
+              <NavItem eventKey={1} href="#">
+                Link
+              </NavItem>
+              <NavItem eventKey={2} href="#">
+                Link
+              </NavItem>
+              <NavDropdown eventKey={3} title="Dropdown" id="basic-nav-dropdown">
+                <MenuItem eventKey={3.1}>Action</MenuItem>
+                <MenuItem eventKey={3.2}>Another action</MenuItem>
+                <MenuItem eventKey={3.3}>Something else here</MenuItem>
+                <MenuItem divider />
+                <MenuItem eventKey={3.4}>Separated link</MenuItem>
+              </NavDropdown>
+            </Nav>
+          </Navbar>;
+        </div>
+
+      </div>
+    );
+  }
+}
